473e9e938f0ca7004b6a1685a6a255f4dd9aec76
Nico Duldhardt <nd12toje@studserv.uni-leipzig.de>
2018-02-12 15:53:41 +0100

    implemented getMedian

--- src/main/scala/de/uni_leipzig/dbs/DecisionTreeTrainer.scala
+++ src/main/scala/de/uni_leipzig/dbs/DecisionTreeTrainer.scala
@@ -11,6 +11,29 @@ import org.apache.flink.util.Collector
 import scala.collection.JavaConverters._
 
 class DecisionTreeTrainer {
+  def getMedian(featureValueData: DataSet[(Int, Double)]): DataSet[(Int, Double)] = {
+    featureValueData
+      .groupingBy {case(feature, value) => feature}
+      .sortGroupWith(Order.ASCENDING) {case(_, value) => value}
+      .reduceGroup(new FeatureMedianFilter)
+  }
+}
 
+class FeatureMedianFilter extends GroupReduceFunction[(Int, Double), (Int, Double)] {
+  override def reduce(it: lang.Iterable[(Int, Double)], out: Collector[(Int, Double)]): Unit = {
+    if(!it.iterator().hasNext) {
+      return
+    }
+    val featureValueVec = it.asScala.toVector
+    val featureIndex = featureValueVec(0)._1
+    val values = featureValueVec.map({case(feature, value) => value})
+
+    if(values.size % 2 == 0) {
+     val median = (values(values.size/2) + values(values.size/2+1))/2
+      out.collect(featureIndex, median)
+    } else {
+      out.collect(featureIndex, values(values.size/2))
+    }
+  }
 }
 
