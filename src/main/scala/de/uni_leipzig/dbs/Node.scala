8bfaf146cd3eee6f148d7bc2ffddfc286304b5d2
Nico Duldhardt <nd12toje@studserv.uni-leipzig.de>
2018-02-15 17:00:30 +0100

    added toString and changed node id to var

--- src/main/scala/de/uni_leipzig/dbs/Node.scala
+++ src/main/scala/de/uni_leipzig/dbs/Node.scala
@@ -1,7 +1,7 @@
 package de.uni_leipzig.dbs
 
 class Node(
-            val id: Int,
+            var id: Int,
             var prediction: Double,
             var predictionProbability: Double,
             var featureIndex: Option[Int],
@@ -9,7 +9,18 @@ class Node(
             var leftChild: Option[Node],
             var rightChild: Option[Node],
             var isLeaf: Boolean
-          ) {
+          ) extends Serializable {
+
+  def this() = this(-1, -1, -1, None, None, None, None, true)
+
+  override def toString: String = {
+    val newLine = System.getProperty("line.seperator")
+    val leftChildNode = leftChild.getOrElse("NoChild").toString
+    val rightChildNode = rightChild.getOrElse("NoChild").toString
+    s"$id, label: $prediction, probability: $predictionProbability, threshold: $threshold, featureIndex: $featureIndex " +
+      s"\n$leftChildNode" +
+      s"\n$rightChildNode"
+  }
 
   def predict(features: Vector[Double]): Double = {
     if(isLeaf) {
