9b44ffe069c3236a1a8723a983446edf1cac43b1
Nico Duldhardt <nd12toje@studserv.uni-leipzig.de>
2018-02-18 19:50:44 +0100

    added scala and java test

---
+++ src/test/scala/de/uni_leipzig/dbs/DecisionTreeTrainerTest.scala
@@ -0,0 +1,46 @@
+package de.uni_leipzig.dbs
+
+import java.net.URLDecoder
+
+import org.apache.flink.api.scala._
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class DecisionTreeTest extends FlatSpec with Matchers {
+
+  behavior of "a decision tree"
+
+  it should "not throw an error" in {
+    val filepathTraining = URLDecoder.decode(getClass().getResource("/musicbrainz/training_musicbrainz_softTFIDF[1_4].csv").toURI().toString, "UTF-8")
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val inputFull: DataSet[(Int, Int, Boolean, Double, Double, Double)] = env.readCsvFile(filepathTraining, fieldDelimiter = ";", ignoreFirstLine = true)
+    val input = inputFull
+
+    val data = input.map(t => {
+      ( {
+        if (t._3) 1.0 else -1.0
+      }, Vector(t._4, t._5, t._6))
+    })
+
+    val model = new DecisionTreeModel fit(data)
+
+
+
+    val filepathTest = URLDecoder.decode(getClass().getResource("/musicbrainz/training_musicbrainz_softTFIDF[1_4].csv").toURI().toString, "UTF-8")
+    val testFull: DataSet[(Int, Int, Boolean, Double, Double, Double)] = env.readCsvFile(filepathTest, fieldDelimiter = ";", ignoreFirstLine = true)
+    val test = testFull
+
+    val testData = test.map(t => {
+      ( {
+        if (t._3) 1.0 else -1.0
+      }, Vector(t._4, t._5, t._6))
+    })
+
+    println("(Accuracy, Precision, Recall): " + model.evaluateBinaryClassification(testData))
+
+
+
+
+
+  }
+}
---
+++ src/test/scala/de/uni_leipzig/dbs/JavaDecisionTreeTest.java
@@ -0,0 +1,69 @@
+package de.uni_leipzig.dbs;
+
+import de.uni_leipzig.dbs.api.java.DecisionTree;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple4;
+
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.Vector;
+
+public class JavaDecisionTreeTest {
+
+    public static void main(String[] args) throws Exception {
+
+        // emulates local environment on java collections for improved performance
+        ExecutionEnvironment env = ExecutionEnvironment.createCollectionsEnvironment();
+        //ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        String filepath = URLDecoder.decode(JavaDecisionTreeTest.class.getResource("/musicbrainz/training_musicbrainz_softTFIDF[1_4].csv").toURI().getPath(), "UTF-8");
+        DataSet<Tuple4<Boolean, Double, Double, Double>> data = env.readCsvFile(filepath)
+                .ignoreFirstLine()
+                .fieldDelimiter(";")
+                .includeFields(false, false, true, true, true, true)
+                .types(Boolean.class, Double.class, Double.class, Double.class);
+
+
+        DataSet<Tuple2<Double, Vector<Double>>> dataLV = data.map(new RawDataToInput());
+        DecisionTree model = new DecisionTree().fit(dataLV);
+
+        DataSet<Vector<Double>> dataV = dataLV.map(new LabelVectorToVector());
+        DataSet<Tuple2<Double, Vector<Double>>> predictedData = model.predict(dataV);
+
+        model.evaluate(dataLV).print();
+        System.out.println("(Accuracy, Precision, Recall): " + model.evaluateBinaryClassification(dataLV));
+
+
+
+        // *************************************************************************
+        //     USER FUNCTIONS
+        // *************************************************************************
+
+    }
+
+    public static final class RawDataToInput implements MapFunction<Tuple4<Boolean, Double, Double, Double>, Tuple2<Double, Vector<Double>>> {
+        @Override
+        public Tuple2<Double, Vector<Double>> map(Tuple4<Boolean, Double, Double, Double> t) throws Exception {
+            Double[] values = new Double[3];
+            Double label;
+            if(t.f0) label = 1.0;
+            else label = -1.0;
+            values[0] = t.f1;
+            values[1] = t.f2;
+            values[2] = t.f3;
+            Vector<Double> vec = new Vector<Double>(Arrays.asList(values));
+            return new Tuple2(label, vec);
+        }
+    }
+
+    public static final class LabelVectorToVector implements MapFunction<Tuple2<Double, Vector<Double>>, Vector<Double>> {
+        @Override
+        public Vector<Double> map(Tuple2<Double, Vector<Double>> value) throws Exception {
+            return value.f1;
+        }
+    }
+
+}
